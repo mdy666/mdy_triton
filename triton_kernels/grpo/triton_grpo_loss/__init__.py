856e86a0a5e1edbdc64062e63769d3bf9e819c20
mdy666 <maduyue1031@gmail.com>
2025-04-24 17:28:57 +0800

    fix bug

--- triton_kernels/grpo/triton_grpo_loss/__init__.py
+++ triton_kernels/grpo/triton_grpo_loss/__init__.py
@@ -39,9 +39,7 @@ def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=N
                                                                 self.beta,
                                                                 self.epsilon_low,
                                                                 self.epsilon_high,)
-     
-    logits.data = torch.Tensor()
-    del logits
+    
     loss = (per_token_loss * completion_mask).sum() / completion_mask.sum()
 
     # Log the metrics
@@ -53,8 +51,97 @@ def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=N
 
     clip_ratio = (is_clipped * completion_mask).sum() / completion_mask.sum()
     self._metrics[mode]["clip_ratio"].append(self.accelerator.gather_for_metrics(clip_ratio).mean().item())
-    return loss
+    return loss, logits
+
+from transformers.trainer import (is_sagemaker_mp_enabled, 
+                                  smp_forward_backward,
+                                  is_torch_xpu_available,
+                                  is_torch_mlu_available,
+                                  is_torch_musa_available,
+                                  is_torch_npu_available,
+                                  is_torch_mps_available,
+                                  OptimizerNames,
+                                  DistributedType
+                                  )
+def training_step(
+    self, model, inputs, num_items_in_batch=None
+) -> torch.Tensor:
+    """
+    Perform a training step on a batch of inputs.
+
+    Subclass and override to inject custom behavior.
+
+    Args:
+        model (`nn.Module`):
+            The model to train.
+        inputs (`Dict[str, Union[torch.Tensor, Any]]`):
+            The inputs and targets of the model.
+
+            The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+            argument `labels`. Check your model's documentation for all accepted arguments.
+
+    Return:
+        `torch.Tensor`: The tensor with training loss on this batch.
+    """
+    model.train()
+    if hasattr(self.optimizer, "train") and callable(self.optimizer.train):
+        self.optimizer.train()
+
+    inputs = self._prepare_inputs(inputs)
+    if is_sagemaker_mp_enabled():
+        loss_mb = smp_forward_backward(model, inputs, self.args.gradient_accumulation_steps)
+        return loss_mb.reduce_mean().detach().to(self.args.device)
+
+    with self.compute_loss_context_manager():
+        loss, logits = self.compute_loss(model, inputs, num_items_in_batch=num_items_in_batch)
+
+    del inputs
+    if (
+        self.args.torch_empty_cache_steps is not None
+        and self.state.global_step % self.args.torch_empty_cache_steps == 0
+    ):
+        if is_torch_xpu_available():
+            torch.xpu.empty_cache()
+        elif is_torch_mlu_available():
+            torch.mlu.empty_cache()
+        elif is_torch_musa_available():
+            torch.musa.empty_cache()
+        elif is_torch_npu_available():
+            torch.npu.empty_cache()
+        elif is_torch_mps_available(min_version="2.0"):
+            torch.mps.empty_cache()
+        else:
+            torch.cuda.empty_cache()
+
+    kwargs = {}
+
+    # For LOMO optimizers you need to explicitly use the learnign rate
+    if self.args.optim in [OptimizerNames.LOMO, OptimizerNames.ADALOMO]:
+        kwargs["learning_rate"] = self._get_learning_rate()
+
+    if self.args.n_gpu > 1:
+        loss = loss.mean()  # mean() to average on multi-gpu parallel training
+
+    if self.use_apex:
+        with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+            scaled_loss.backward()
+    else:
+        # Finally we need to normalize the loss for reporting
+        if not self.model_accepts_loss_kwargs and self.compute_loss_func is None:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        # Turning off loss scaling w.r.t. gradient accumulation when DeepSpeed is enabled
+        # https://github.com/huggingface/transformers/pull/35808
+        if self.accelerator.distributed_type == DistributedType.DEEPSPEED:
+            kwargs["scale_wrt_gas"] = False
+
+        self.accelerator.backward(loss, **kwargs)
+        
+        logits.data = torch.Tensor()
+        del logits
+        return loss.detach()
 
 trl.GRPOTrainer._get_per_token_logps = _get_per_token_logps
 trl.GRPOTrainer.compute_loss = compute_loss
+trl.GRPOTrainer.training_step = training_step
 trigger = None
