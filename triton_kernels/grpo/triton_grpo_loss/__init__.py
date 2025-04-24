3b5861bfda79e50dc299d706f861466871c6dfa5
mdy666 <maduyue1031@gmail.com>
2025-04-24 17:42:09 +0800

    fix grpo bug

--- triton_kernels/grpo/triton_grpo_loss/__init__.py
+++ triton_kernels/grpo/triton_grpo_loss/__init__.py
@@ -53,44 +53,18 @@ def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=N
     self._metrics[mode]["clip_ratio"].append(self.accelerator.gather_for_metrics(clip_ratio).mean().item())
     return loss, logits
 
-from transformers.trainer import (is_sagemaker_mp_enabled, 
-                                  smp_forward_backward,
-                                  is_torch_xpu_available,
-                                  is_torch_mlu_available,
-                                  is_torch_musa_available,
-                                  is_torch_npu_available,
-                                  is_torch_mps_available,
+from transformers.trainer import (
                                   OptimizerNames,
                                   DistributedType
                                   )
 def training_step(
     self, model, inputs, num_items_in_batch=None
 ) -> torch.Tensor:
-    """
-    Perform a training step on a batch of inputs.
-
-    Subclass and override to inject custom behavior.
-
-    Args:
-        model (`nn.Module`):
-            The model to train.
-        inputs (`Dict[str, Union[torch.Tensor, Any]]`):
-            The inputs and targets of the model.
-
-            The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
-            argument `labels`. Check your model's documentation for all accepted arguments.
-
-    Return:
-        `torch.Tensor`: The tensor with training loss on this batch.
-    """
     model.train()
     if hasattr(self.optimizer, "train") and callable(self.optimizer.train):
         self.optimizer.train()
 
     inputs = self._prepare_inputs(inputs)
-    if is_sagemaker_mp_enabled():
-        loss_mb = smp_forward_backward(model, inputs, self.args.gradient_accumulation_steps)
-        return loss_mb.reduce_mean().detach().to(self.args.device)
 
     with self.compute_loss_context_manager():
         loss, logits = self.compute_loss(model, inputs, num_items_in_batch=num_items_in_batch)
@@ -100,18 +74,7 @@ def training_step(
         self.args.torch_empty_cache_steps is not None
         and self.state.global_step % self.args.torch_empty_cache_steps == 0
     ):
-        if is_torch_xpu_available():
-            torch.xpu.empty_cache()
-        elif is_torch_mlu_available():
-            torch.mlu.empty_cache()
-        elif is_torch_musa_available():
-            torch.musa.empty_cache()
-        elif is_torch_npu_available():
-            torch.npu.empty_cache()
-        elif is_torch_mps_available(min_version="2.0"):
-            torch.mps.empty_cache()
-        else:
-            torch.cuda.empty_cache()
+        torch.cuda.empty_cache()
 
     kwargs = {}
 
@@ -122,24 +85,20 @@ def training_step(
     if self.args.n_gpu > 1:
         loss = loss.mean()  # mean() to average on multi-gpu parallel training
 
-    if self.use_apex:
-        with amp.scale_loss(loss, self.optimizer) as scaled_loss:
-            scaled_loss.backward()
-    else:
-        # Finally we need to normalize the loss for reporting
-        if not self.model_accepts_loss_kwargs and self.compute_loss_func is None:
-            loss = loss / self.args.gradient_accumulation_steps
+    # Finally we need to normalize the loss for reporting
+    if not self.model_accepts_loss_kwargs and self.compute_loss_func is None:
+        loss = loss / self.args.gradient_accumulation_steps
 
-        # Turning off loss scaling w.r.t. gradient accumulation when DeepSpeed is enabled
-        # https://github.com/huggingface/transformers/pull/35808
-        if self.accelerator.distributed_type == DistributedType.DEEPSPEED:
-            kwargs["scale_wrt_gas"] = False
+    # Turning off loss scaling w.r.t. gradient accumulation when DeepSpeed is enabled
+    # https://github.com/huggingface/transformers/pull/35808
+    if self.accelerator.distributed_type == DistributedType.DEEPSPEED:
+        kwargs["scale_wrt_gas"] = False
 
-        self.accelerator.backward(loss, **kwargs)
-        
-        logits.data = torch.Tensor()
-        del logits
-        return loss.detach()
+    self.accelerator.backward(loss, **kwargs)
+    
+    logits.data = torch.Tensor()
+    del logits
+    return loss.detach()
 
 trl.GRPOTrainer._get_per_token_logps = _get_per_token_logps
 trl.GRPOTrainer.compute_loss = compute_loss
